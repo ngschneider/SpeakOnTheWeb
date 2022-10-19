4ff3d1127350166e8f57bba7bb38a171694f7403
Nolan Schneider <ngschneider@unomaha.edu>
2022-10-19 17:46:58 -0500

    Add files via upload

---
+++ webDriver.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import SeleniumUtilities as selUtil
+import time
+
+## LOCATION OF WEB DRIVER
+PATH = "C:\\Users\\Nolan Schneider\\Downloads\\chromedriver_win32 (1)\chromedriver"
+URL = "https://Google.com"
+selUtil.openBrowswer(PATH)
+selUtil.openSite(URL)
+selUtil.refreshPage()
+selUtil.openSite("https://Twitter.com")
+time.sleep(60)
+selUtil.navBack()
+
+#assert "Python" in driver.title
+#elem = driver.find_element(By.NAME, "q")
+#elem.clear()
+#elem.send_keys("pycon")
+#elem.send_keys(Keys.RETURN)
+#assert "No results found." not in driver.page_source
+#driver.close()
