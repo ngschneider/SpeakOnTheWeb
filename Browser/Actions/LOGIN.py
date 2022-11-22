1a707a51fc602e68d7b3e1b4fb073ad7f987ede2
Nolan Schneider <nolan.schneider@huskers.unl.edu>
2022-11-21 21:30:06 -0600

    LOGIN and comments

--- Browser/Actions/LOGIN.py
+++ Browser/Actions/LOGIN.py
@@ -1,40 +1,62 @@
-### PROCEDURE TO LOGIN FROM A FORM ###
+#### LOGIN REQUIRMENTS ####
+### LOGIN IS SITE SPECIFIC, SO THE FORM ELEMENTS NEED TO BE KNOW FOR EACH SITE.
 
-userElmementName= 'UnoUserName'
-passElementName= ''
+# KEY ARE SITES THAT HAVE BEEN IMPLEMENTED, VALUE IS THE NORMAILZED NAME; example, youtube.com -> youtube
+loginable = {
+    'youtube.com' : 'youtube',
+    'youtube' : 'youtube',
+    'google' : 'google'
+}
 
-username = ''
-password = ''
+# Key is the site to LOGIN, Value a truple of the element type and element name
+# Should be in the form (TYPE,username,Type, password TYPE, subbit)
+elementTag = {
+    'youtube' : ('NAME', 'search_query', "ID", 'search-icon-legacy'),
+    'google' : ('NAME', 'q','NAME', 'btnK' )
+}
 
-def findUserElementName():
-    pass
+# FUNCTION TO DETERMINE IF THE SITE BEEN HAS IMPLEMENTED
+# RETURNS 
+#   Tuple
+#       Boolean : True if the site has been implemented
+#       String : Name of site 
+def isLogin(site: str):
+    if(site in loginable):
+        return (True,loginable.get(site))
+    else:
+        return (False,"")
 
-def findPassordElementName():
-    pass
 
-def findLoginElements():
-    pass
+# FUNCTION to get string of site with only the name of the site
+def normalizeSite(site: str):
+    locationLeft = -1
+    locationRight = -1
+    locationLeft = site.find("www.")
+    locationRight = site.find(".com")
+    return site[locationLeft+4:locationRight]
 
-def setUsername(user):
-    username = user
-
-def setpassword(mypass):
-    password = mypass
+# Returns string of the command with spaces
+def textToSearch(command):
+    text: str = ""
+    for x in command:
+        if x != "search":
+            text = text + " " + x
+    return text
     
-# Ask the user what their username is
-def getUsername():
-    pass
+# FUNCTION to get all required Information to search a website
+# RETURNS
+#   Tuple
+#       Boolean : True if site can be searched
+#       Tuple : (str: Element type,str: Element Name)
+#           
+def requirment(site: str, command):
+    siteName = normalizeSite(site)
+    searchRequirment = isLogin(siteName)
+    text = textToSearch(command)
+    if searchRequirment[0]: 
+        elementInformation = elementTag.get(searchRequirment[1]) 
+        return (True, text, elementInformation)
+    else:
+        return (False, "", ("",""))
 
-# Ask the user what their username is
-def getpassword():
-    pass
-
-def login():
-    # Enter username
-    driver.find_element(By.ID, userElmementName)
-    driver.send_keys(username)
-    # Enter password
-    driver.find_element(By.ID, passElementName)
-    driver.send_keys(password)
-    # Submit form
-    driver.find_element(By.ID, "submit")
+print(textToSearch(["search", "1", "2", "3"]))
